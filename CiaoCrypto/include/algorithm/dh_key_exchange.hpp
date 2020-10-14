93948648661670a603ef342316e8fc7071436cd9
ouchiminh <39828763+ouchiminh@users.noreply.github.com>
2020-10-14 22:35:16 +0900

    update

--- CiaoCrypto/include/algorithm/dh_key_exchange.hpp
+++ CiaoCrypto/include/algorithm/dh_key_exchange.hpp
@@ -1,12 +1,32 @@
-﻿#include <type_traits>
+﻿#include "ouchilib/math/modint.hpp"
+#include <type_traits>
 
 namespace ciao {
 
-template<class T>
-concept integer = std::is_integral_v<T>;
+template<class Int>
+class dh_key_exchange {
+public:
+    dh_key_exchange(Int g, Int p, Int private_key)
+        : g_{g, p}, private_key_{private_key}
+    {
+        namespace mp = boost::multiprecision;
+        mp::miller_rabin()
+    }
 
-template<integer Int>
-class dh_key_exchange;
+    Int get_public_key() const
+    {
+        return pow(g_, private_key_)
+    }
+    Int calc_secret(Int bobs_public_key) const
+    {
+        return pow(modint<Int>(bobs_public_key, g_.mod()), private_key_);
+    }
+    
+
+private:
+    ouchi::math::modint<Int> g_;
+    Int private_key_;
+};
 
 }
 
