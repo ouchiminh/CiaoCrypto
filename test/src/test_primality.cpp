8e81a6c5e367476209d8c054f8dff6ce81d0c5d8
ouchiminh <39828763+ouchiminh@users.noreply.github.com>
2020-10-23 18:40:40 +0900

    impl sha

--- test/src/test_primality.cpp
+++ test/src/test_primality.cpp
@@ -2,15 +2,17 @@
 #include "primality.hpp"
 #include "algorithm/dh_key_exchange.hpp"
 
+#ifdef NDEBUG
 OUCHI_TEST_CASE(test_prime_gen)
 {
     using namespace ciao;
-    auto p = random_choice<2048>();
+    auto p = random_choice<1024>();
     dh_key_exchange<decltype(p)> dha(2, p, 40);
     dh_key_exchange<decltype(p)> dhb(2, p, 13);
-    std::cout << p << '\n';
+    //std::cout << p << '\n';
     OUCHI_CHECK_EQUAL(
         dhb.calc_secret(dha.get_public_key()),
         dha.calc_secret(dhb.get_public_key()));
 }
+#endif
 
---
+++ test/src/test_sha.cpp
@@ -0,0 +1,63 @@
+﻿#include <chrono>
+#include "../ouchitest/include/ouchitest.hpp"
+#include "algorithm/sha.hpp"
+
+OUCHI_TEST_CASE(test_sha256)
+{
+    using namespace ciao;
+    sha<256> sha256;
+    sha256.update("abc");
+    auto res = sha256.finalize();
+    OUCHI_CHECK_EQUAL(res.byte_data[0], 0xba);
+    sha256.update("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq");
+    sha256.finalize(res.byte_data);
+    OUCHI_CHECK_EQUAL(res.byte_data[0], 0x24);
+}
+
+#ifdef NDEBUG
+OUCHI_TEST_CASE(benchmark_sha256)
+{
+    namespace chrono = std::chrono;
+    using namespace ciao;
+    constexpr int w = 1024 * 1024 * 64;
+    static char data[16384] = {};
+    sha<256> sha256;
+    {
+        constexpr int c = 16;
+        constexpr int r = w / c;
+        auto beg = chrono::steady_clock::now();
+        for (int i = 0; i < r; ++i) {
+            sha256.update(data, c);
+            (void)sha256.finalize();
+        }
+        auto end = chrono::steady_clock::now();
+        chrono::duration<double> diff = end - beg;
+        std::cout << "sha256 " << c << "bytes " << w / diff.count() / 1024 << "kB/s\n";
+    }
+    {
+        constexpr int c = 1024;
+        constexpr int r = w / c;
+        auto beg = chrono::steady_clock::now();
+        for (int i = 0; i < r; ++i) {
+            sha256.update(data, c);
+            (void)sha256.finalize();
+        }
+        auto end = chrono::steady_clock::now();
+        chrono::duration<double> diff = end - beg;
+        std::cout << "sha256 " << c << "bytes " << w / diff.count() / 1024 << "kB/s\n";
+    }
+    {
+        constexpr int c = 16384;
+        constexpr int r = w / c;
+        auto beg = chrono::steady_clock::now();
+        for (int i = 0; i < r; ++i) {
+            sha256.update(data, c);
+            (void)sha256.finalize();
+        }
+        auto end = chrono::steady_clock::now();
+        chrono::duration<double> diff = end - beg;
+        std::cout << "sha256 " << c << "bytes " << w / diff.count() / 1024 << "kB/s\n";
+    }
+}
+#endif
+
