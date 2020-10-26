bf149581952a9c5370da72f1f1c723a2324db67a
ouchiminh <39828763+ouchiminh@users.noreply.github.com>
2020-10-26 19:14:50 +0900

    array shift
    added bit shift in array

--- CiaoCrypto/include/common.hpp
+++ CiaoCrypto/include/common.hpp
@@ -1,6 +1,8 @@
 ﻿#pragma once
 #include <type_traits>
 #include <cstdint>
+#include <cstring>
+#include <cassert>
 
 namespace ciao {
 template<class Int>
@@ -62,6 +64,79 @@ inline std::uint32_t memassign32(std::uint8_t a,
     r.b[3] = d;
     return r.i;
 }
+
+template<class Int, size_t S>
+inline auto shiftr_array(Int (&data)[S], unsigned bit_width)
+-> std::enable_if_t<std::is_unsigned_v<Int> && sizeof(Int) == 1>
+{
+    assert(S * CHAR_BIT >= bit_width && CHAR_BIT == 8);
+
+    auto elm_shift_width = (int)bit_width >> 3;
+    auto bit_shift_mod_width = (int)bit_width & (CHAR_BIT - 1);    // bit_width mod 8
+
+    Int buf[S] = {};
+
+    constexpr std::uint8_t lowbit[] = { 0,1,3,7,15,31,63,127 };
+    auto s = lowbit[bit_shift_mod_width];
+    auto f = ~s;
+
+    for (unsigned i = 0; i < S - elm_shift_width; ++i) {
+        if(i + elm_shift_width + 1 < S)
+            buf[i + elm_shift_width + 1] |= (data[i] & s) << (CHAR_BIT - bit_shift_mod_width);
+        buf[i + elm_shift_width] |= (data[i] & f) >> bit_shift_mod_width;
+    }
+    std::memcpy(data, buf, S);
+}
+
+template<class Int, size_t S>
+inline auto shiftl_array(Int (&data)[S], unsigned bit_width)
+-> std::enable_if_t<std::is_unsigned_v<Int> && sizeof(Int) == 1>
+{
+    assert(S * CHAR_BIT >= bit_width && CHAR_BIT == 8);
+
+    auto elm_shift_width = (int)bit_width >> 3;
+    auto bit_shift_mod_width = (int)bit_width & (CHAR_BIT - 1);    // bit_width mod 8
+
+    Int buf[S] = {};
+
+    constexpr std::uint8_t lowbit[] = { 0,1,3,7,15,31,63,127 };
+    auto s = ~lowbit[bit_shift_mod_width];
+    auto f = ~s;
+
+    for (int i = elm_shift_width; i < (int)S; ++i) {
+        if(i - elm_shift_width - 1 >= 0)
+            buf[i - elm_shift_width - 1] |= (data[i] & s) >> (CHAR_BIT - bit_shift_mod_width);
+        buf[i - elm_shift_width] |= (data[i] & f) << bit_shift_mod_width;
+    }
+    std::memcpy(data, buf, S);
+}
+
+template<class Int, size_t S>
+inline auto rotl_array(Int(&bits)[S], unsigned bit_width)
+-> std::enable_if_t<std::is_unsigned_v<Int> && sizeof(Int) == 1>
+{
+    constexpr auto w = CHAR_BIT * S;
+    //return (x << nbit) | (x >> (w - nbit));
+    Int buf[S];
+    std::memcpy(buf, bits, S);
+    shiftl_array(bits, bit_width);
+    shiftr_array(buf, w - bit_width);
+    for (unsigned i = 0; i < S; ++i) bits[i] |= buf[i];
+}
+
+template<class Int, size_t S>
+inline auto rotr_array(Int(&bits)[S], unsigned bit_width)
+-> std::enable_if_t<std::is_unsigned_v<Int> && sizeof(Int) == 1>
+{
+    constexpr auto w = CHAR_BIT * S;
+    //return (x << nbit) | (x >> (w - nbit));
+    Int buf[S];
+    std::memcpy(buf, bits, S);
+    shiftr_array(bits, bit_width);
+    shiftl_array(buf, w - bit_width);
+    for (unsigned i = 0; i < S; ++i) bits[i] |= buf[i];
+}
+
 
 }
 
---
+++ test/src/test_bit_op.cpp
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "../ouchitest/include/ouchitest.hpp"
+#include "common.hpp"
+
+OUCHI_TEST_CASE(test_shiftr_array)
+{
+    unsigned char data[] = { 1, 127 << 1, 0 };
+    ciao::shiftr_array(data, 9);
+    OUCHI_CHECK_EQUAL((unsigned)data[2], 255u);
+    OUCHI_CHECK_EQUAL(data[1], 0u);
+    OUCHI_CHECK_EQUAL(data[0], 0u);
+}
+
+OUCHI_TEST_CASE(test_shiftl_array)
+{
+    unsigned char data[] = { 0xf1,0xfc };
+    ciao::shiftl_array(data, 4);
+    OUCHI_CHECK_EQUAL((unsigned)data[0], 0x1fu);
+    OUCHI_CHECK_EQUAL((unsigned)data[1], 0xc0u);
+}
+
