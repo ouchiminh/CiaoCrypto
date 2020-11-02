47cbf4db7e6778e8dd339c913027cc75f3966959
ouchiminh <39828763+ouchiminh@users.noreply.github.com>
2020-11-02 19:15:00 +0900

    Update test_camellia.cpp

--- test/src/test_camellia.cpp
+++ test/src/test_camellia.cpp
@@ -156,7 +156,7 @@ OUCHI_TEST_CASE(benchmark_camellia128_ctr_raw)
     using namespace std::chrono;
     const unsigned char key[] = "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f";
     constexpr int r = 1024 * 8;
-    constexpr int c = 4 * 1024;
+    constexpr int c = 32 * 1024;
     ciao::camellia<16> encoder{ key };
     union {
         std::uint64_t q[2];
