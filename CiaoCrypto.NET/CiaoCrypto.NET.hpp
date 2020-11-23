5b76743facd868ee735937e8203c1009583b5ac2
ouchiminh <39828763+ouchiminh@users.noreply.github.com>
2020-11-23 19:33:21 +0900

    add sha

--- CiaoCrypto.NET/CiaoCrypto.NET.hpp
+++ CiaoCrypto.NET/CiaoCrypto.NET.hpp
@@ -3,6 +3,5 @@
 #include "block_cipher_mode.net.hpp"
 #include "block_cipher_algorithm.hpp"
 #include "rsa.hpp"
-
-using namespace System;
+#include "sha.hpp"
 
---
+++ CiaoCrypto.NET/sha.hpp
@@ -0,0 +1,58 @@
+﻿#include "algorithm/sha.hpp"
+
+namespace cn {
+
+public ref class sha256 {
+public:
+    sha256()
+        : hash_{ new ciao::sha256() }
+    {}
+
+    void update(cli::array<Byte>^ data)
+    {
+        cli::pin_ptr<Byte> ptr = &data[0];
+        hash_->update(ptr, data->Length);
+    }
+    cli::array<Byte>^ finalize()
+    {
+        cli::array<Byte>^ dest = gcnew cli::array<Byte>(256);
+        finalize(dest);
+        return dest;
+    }
+    void finalize(cli::array<Byte>^ dest){
+        assert(dest->Length > 256);
+        cli::pin_ptr<Byte> ptr = &dest[0];
+        hash_->finalize(ptr);
+    }
+
+private:
+    ciao::sha256* hash_;
+};
+public ref class sha512 {
+public:
+    sha512()
+        : hash_{ new ciao::sha512() }
+    {}
+
+    void update(cli::array<Byte>^ data)
+    {
+        cli::pin_ptr<Byte> ptr = &data[0];
+        hash_->update(ptr, data->Length);
+    }
+    cli::array<Byte>^ finalize()
+    {
+        cli::array<Byte>^ dest = gcnew cli::array<Byte>(512);
+        finalize(dest);
+        return dest;
+    }
+    void finalize(cli::array<Byte>^ dest){
+        assert(dest->Length > 512);
+        cli::pin_ptr<Byte> ptr = &dest[0];
+        hash_->finalize(ptr);
+    }
+
+private:
+    ciao::sha512* hash_;
+};
+}
+
